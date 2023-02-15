1fb639c76a7eb7a3c32873530847b68ea51a90f8
Jackson Price <price.jacksonk@gmail.com>
2023-02-15 16:40:35 -0600

    Add: find and save new members when bot comes online

--- app/sidekiq/discord_bot_job.rb
+++ app/sidekiq/discord_bot_job.rb
@@ -3,12 +3,23 @@ class DiscordBotJob
   require 'discordrb'
 
   def self.perform
-    channel_id = 1074904173381951508
     bot = Discordrb::Bot.new token: Rails.application.credentials.dig(:discord, :token)
     bot.ready do |event|
       puts "Logged in as #{bot.profile.username} (ID:#{bot.profile.id}) | #{bot.servers.size} servers".green
       puts 'Looking for new members...'.yellow
       find_and_save_new_members(bot)
+    end
+
+    bot.voice_state_update do |event|
+      if event.old_channel.nil? && !event.channel.nil?
+        # A user has joined a voice channel
+        puts "User #{event.user.name} joined voice channel #{event.channel.name} at #{DateTime.now}"
+        # You can replace the above line with code to insert the data into your database
+      elsif !event.old_channel.nil? && event.channel.nil?
+        # A user has left a voice channel
+        puts "User #{event.user.name} left voice channel #{event.old_channel.name} at #{DateTime.now}"
+        # You can replace the above line with code to insert the data into your database
+      end
     end
 
     bot.run
@@ -18,14 +29,21 @@ class DiscordBotJob
     server = bot.server(669719500504956958)
     members = server.members
 
-    members.each do |member|
-      next if Member.find_by(discord_id: member.id)
-      puts 'New member found... persisting to database.'.yellow
-      if Member.create(discord_id: member.id, display_name: member.display_name)
-        puts "#{member.display_name} has been saved".green
-      else
-        puts 'There was an error saving the new member.'.red
+    current_members_count = members.count
+    saved_members_count = Member.all.count
+
+    if saved_members_count < current_members_count
+      members.each do |member|
+        next if Member.find_by(discord_id: member.id)
+        puts 'New member found... persisting to database.'.yellow
+        if Member.create(discord_id: member.id, display_name: member.display_name)
+          puts "#{member.display_name} has been saved".green
+        else
+          puts 'There was an error saving the new member.'.red
+        end
       end
+    else
+      puts 'No new members found.'.green
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'bot_settings/index'
+  get 'bot_settings/start_bot'
   resources :members
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
