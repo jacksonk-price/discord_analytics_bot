b30176712ec1d2e48b353c9d172860e9dd823ce0
Jackson Price <price.jacksonk@gmail.com>
2023-02-17 17:40:27 -0600

    Add: Member service

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,2 @@
 class Member < ApplicationRecord
-
 end
--- app/services/member_service.rb
+++ app/services/member_service.rb
@@ -1,5 +1,26 @@
-class MemberService
-  def self.save_server_member(member)
-    # save member to member model from here
+module MemberService
+  def save_server_member(member)
+    Member.create(discord_id: member.id, display_name: member.display_name)
+  end
+
+  def save_new_members
+    members = @server.members
+
+    current_members_count = members.count
+    saved_members_count = Member.all.count
+
+    if saved_members_count < current_members_count
+      members.each do |member|
+        next if Member.find_by(discord_id: member.id)
+        puts 'New member found... persisting to database.'.yellow
+        if save_server_member(member)
+          puts "#{member.display_name} has been saved".green
+        else
+          puts 'There was an error saving the new member.'.red
+        end
+      end
+    else
+      puts 'No new members found.'.green
+    end
   end
 end
